403fcef78c9617e33744630b835ea043c446a124
wuguanyiyi <wuguanguanyi@gmail.com>
2023-02-21 17:08:25 +0800

    加入專案檔案。

---
+++ even-odd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace even_odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //輸入一串以逗號分隔的整數數字字串，將其依照奇偶數分割為兩個數列，並且予以排序顯示結果
+            Console.WriteLine("輸入一段字串以逗號分隔");
+            
+
+            string[] num = Console.ReadLine().Split(',');
+
+            var even = num.Where(x => int.Parse(x) % 2 ==0);
+            var odd = num.Where(x => int.Parse(x) % 2 != 0);
+            Console.WriteLine($"偶數:{string.Join(",",even)}"); //string.Join 串連字串，並分隔符號
+            Console.WriteLine($"奇數:{string.Join(",",odd)}");
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 序列倒置1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 序列倒置1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //輸入一串以逗號分隔的字串，並將其反向顯示
+            Console.WriteLine("請輸入一串字串，以逗號分隔");
+            string number = Console.ReadLine();
+            
+            string [] num = number.Split(',');
+            var result = num.Reverse(); //Reverse 反轉陣列順序
+
+            Console.WriteLine(string.Join(",", result));
+
+
+            Console.ReadKey();
+        }
+        
+    }
+    
+}
